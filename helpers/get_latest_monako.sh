16e12428d8fbcfc304b6c6fed8053ed6a46ec301
Matthias Küch <mail@matthias-kuech.de>
2020-04-20 20:03:13 +0200

    Always use latest Monako

---
+++ helpers/get_latest_monako.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fetches and unpacks the latest monako release
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/snipem/monako/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/' |
+    sed 's/^v//g'
+}
+
+latest_release=$(get_latest_release)
+
+wget "https://github.com/snipem/monako/releases/download/v${latest_release}/monako_${latest_release}_linux_amd64.tar.gz"
+tar xvf "monako_${latest_release}_linux_amd64.tar.gz"
+chmod +x monako
